746eb1ab3b85bd8bb707a41eeac7f206b8dcffa3
nicholasvvong <nicholasvvong@gmail.com>
2019-11-08 17:26:56 -0800

    First push for database files
    Basic test has been done on the class files. All functions work as of now if nothing is wrong (database down). Need to implement backup system and additional database functions(adding devices, possible deletions).
    Main issue: getting user/password(currently hardcoded). Not sure if going to include in config file or other options

---
+++ DatabaseInfo.cpp
@@ -0,0 +1,137 @@
+#include "DatabaseInfo.h"
+
+using namespace std;
+using namespace pqxx;
+
+DatabaseInfo::DatabaseInfo()
+{
+	databaseAddress = "";
+	databasePort = "";
+	databaseName = "";
+	databaseConnect = "host= " + databaseAddress + " port= " + databasePort + " dbname=" + databaseName + " user=postgres password=library2";
+	
+	connected = false;
+}
+
+DatabaseInfo::DatabaseInfo(string databaseAddress, string databasePort, string databaseName)
+{
+	this->databaseAddress = databaseAddress;
+	this->databasePort = databasePort;
+	this->databaseName = databaseName;
+	
+	databaseConnect = "host= " + databaseAddress + " port= " + databasePort + " dbname=" + databaseName + " user=postgres password=library2";
+	
+	connected = false;
+}
+
+bool DatabaseInfo::connectDatabase()
+{
+	connection c(databaseConnect);
+	if(c.is_open())
+	{
+		connected = true;
+		return true;
+	}
+	else
+	{
+		connected = false;
+		return false;
+	}
+}
+
+bool DatabaseInfo::addNewInfo(int id, int moveIn, int moveOut)
+{
+	string squery = "INSERT INTO locationTimeInfo(id, moveIn, moveOut, time) VALUES (";
+	squery += to_string(id) + ", " + to_string(moveIn) + ", " + to_string(moveOut) + ", clock_timestamp());";
+	
+	try{
+		connection conn(databaseConnect);
+		if(!conn.is_open()){
+			cout << "Failed open" << endl;
+			return false;
+		}
+	
+		work N(conn);
+		N.exec(squery);
+		N.commit();
+		//conn.disconnect();
+	}
+	catch (const exception &e) {
+		cout << "Failed to addNewInfo: " << e.what() << endl;
+		//backupInfo(id, moveIn, moveOut);
+	}
+	
+	updateCurInfo(id, moveIn, moveOut);
+}
+bool DatabaseInfo::updateCurInfo(int id, int moveIn, int moveOut)
+{
+	int *count = new int[2];
+	if(getCurrentCount(id, count))
+	{
+		*(count) += moveIn;
+		*(count + 1) += moveOut;
+		string sql = "UPDATE locationCurInfo SET moveIn = " + to_string(*(count)) + ", moveOut = " + to_string(*(count + 1)) + "WHERE id = " + to_string(id) + ";";
+		try
+		{
+			connection conn(databaseConnect);
+			if(!conn.is_open()){
+				cout << "Failed open" << endl;
+				return false;
+			}
+			
+			work W(conn);
+			W.exec(sql);
+			W.commit();
+		}
+		catch (const exception &e) 
+		{
+			cout << "Erorroor " << e.what() << endl;
+			delete []count;
+			return false;
+		}
+	}
+	else
+	{
+		cout << "Errorr: Failed to getCurrentCount" << endl;
+		delete []count;
+		return false;
+	}
+	delete []count;
+}
+
+
+
+
+bool DatabaseInfo::getCurrentCount(int id, int *count)
+{
+	/*
+	string subsql = "SELECT MAX(time) FROM locationTimeInfo WHERE id = " + id + ";";
+	string sql = "SELECT moveIn, moveOut FROM locationTimeInfo WHERE id = " + id + " AND time = " + subsql + ";";
+	*/
+	string sql = "SELECT moveIn, moveOut FROM locationCurInfo WHERE id = " + to_string(id) + ";";
+	try
+	{
+		connection conn(databaseConnect);
+		if(!conn.is_open()){
+			cout << "Failed open" << endl;
+			return false;
+		}
+		
+		nontransaction N(conn);
+		
+		result R(N.exec(sql));
+			
+		for(result::const_iterator i = R.begin(); i != R.end(); ++i) {
+			//cout << "MoveIn: " << i[0].as<int>() << endl;
+			//cout << "MoveOut: " << i[1].as<int>() << endl;
+			
+			count[0] = i[0].as<int>();
+			count[1] = i[1].as<int>();
+		}
+		return true;
+	}
+	catch (const exception &e) {
+		cout << "Erorroor " << e.what() << endl;
+		return false;
+	}
+}
---
+++ DatabaseInfo.h
@@ -0,0 +1,37 @@
+#ifndef DATABASEINFO_H
+#define DATABASEINFO_H
+
+#include <iostream>
+#include <string>
+#include <pqxx/pqxx>
+
+class DatabaseInfo
+{
+	private:
+		//connection *conn;
+		std::string databaseAddress;
+		std::string databasePort;
+		std::string databaseName;
+		std::string databaseConnect;
+		
+		bool connected;
+		
+		bool getCurrentCount(int id, int *count);
+	public:
+		DatabaseInfo();
+		DatabaseInfo(std::string databaseAddress, std::string databasePort, std::string databaseName);
+		
+		void setDatabaseAddress(std::string databaseAddress);
+		void setDatabasePort(std::string databasePort);
+		void setDatabaseName(std::string databaseName);
+		
+		bool connectDatabase();
+		bool addDevice();
+		bool updateCurInfo();
+		bool addTimeInfo();
+		
+		bool addNewInfo(int id, int moveIn, int moveOut);
+		bool updateCurInfo(int id, int moveIn, int moveOut);
+};
+
+#endif
