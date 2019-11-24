2264a49dd573f283a42d74907747581e52b48d2f
Mike Nguyen <32407498+mikeybear@users.noreply.github.com>
2019-11-24 13:15:04 -0800

    added connection to database

--- hub.php
+++ hub.php
@@ -11,18 +11,72 @@
         <ul class="list">
             <li>Lackluster Library Locator</li>
             <li>2nd floor</li>
-            <li>In: 45</li>
-            <li>Out: 30</li>
+            <li><?php
+            $db_connection = pg_connect("host=localhost dbname=Library user=postgres password=library2")
+            or die('Could not connect: ' . pg_last_error());
+            
+            $query = 'SELECT name FROM username';
+            $usercomp = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+            // output
+            while ($line = pg_fetch_array($usercomp, null, PGSQL_ASSOC))  {
+                //echo  "\t<tr>\n";
+                foreach ($line as $col_value) {
+                    echo "\t\t<td>$col_value</td>\n"; // replace with current data
+                    //echo "Success!\n";
+                }
+                //echo "\t</tr>\n";
+            }
+            // close connection
+                echo "hello";
+            pg_close($db_connection);
+            ?></li>
                 <li><button type="button" onclick="alert('test')"> Download </button></li>
             <hr><br>
             <li>3rd floor</li>
-            <li>In: 37</li>
-            <li>Out: 18</li>
+            <li><?php
+            $db_connection = pg_connect("host=localhost dbname=Library user=postgres password=library2")
+            or die('Could not connect: ' . pg_last_error());
+            
+            $query = 'SELECT name FROM username';
+            $usercomp = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+            // output
+            while ($line = pg_fetch_array($usercomp, null, PGSQL_ASSOC))  {
+                //echo  "\t<tr>\n";
+                foreach ($line as $col_value) {
+                    echo "\t\t<td>$col_value</td>\n"; // replace with current data
+                    //echo "Success!\n";
+                }
+                //echo "\t</tr>\n";
+            }
+            // close connection
+                echo "hello";
+            pg_close($db_connection);
+            ?></li>
                 <li><button type="button" onclick="alert('test2')"> Download </button></li>
             <hr><br>
             <li>4th floor</li>
-            <li>In: 24</li>
-            <li>Out: 11</li>
+            <li><?php
+            $db_connection = pg_connect("host=localhost dbname=Library user=postgres password=library2")
+            or die('Could not connect: ' . pg_last_error());
+            
+            $query = 'SELECT name FROM username';
+            $usercomp = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+            // output
+            while ($line = pg_fetch_array($usercomp, null, PGSQL_ASSOC))  {
+                //echo  "\t<tr>\n";
+                foreach ($line as $col_value) {
+                    echo "\t\t<td>$col_value</td>\n"; // replace with current data
+                    //echo "Success!\n";
+                }
+                //echo "\t</tr>\n";
+            }
+            // close connection
+                echo "hello";
+            pg_close($db_connection);
+            ?></li>
                 <li><button type="button" onclick="alert('test3')"> Download </button></li>
             <hr><br>
             <a href="login.php">Logout</a>
