c31952ac3ab4486ec8d357465a45be33bb6e3e59
Mike Nguyen <32407498+mikeybear@users.noreply.github.com>
2019-12-06 16:49:55 -0800

    deleted comments

--- csv.php
+++ csv.php
@@ -19,23 +19,7 @@ $file = fopen('php://output', 'w');
  
 // send the column headers
 fputcsv($file, array('Column 1', 'Column 2', 'Column 3'));
- 
-// Sample data. This can be fetched from mysql too
-	/*
-$data = array(
-array('Data 11', 'Data 12', 'Data 13', 'Data 14', 'Data 15'),
-array('Data 21', 'Data 22', 'Data 23', 'Data 24', 'Data 25'),
-array('Data 31', 'Data 32', 'Data 33', 'Data 34', 'Data 35'),
-array('Data 41', 'Data 42', 'Data 43', 'Data 44', 'Data 45'),
-array('Data 51', 'Data 52', 'Data 53', 'Data 54', 'Data 55')
-);
- 
-// output each row of the data
-foreach ($data as $row)
-{
-fputcsv($file, $row);
-}
-	*/
+
 while ($line = pg_fetch_array($usercomp, null, PGSQL_ASSOC))  {
 	foreach ($line as $row) {
  		fputcsv($file, $row); // need to test this out with different floors
