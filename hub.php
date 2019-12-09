98c6b78129f48a058862c377d85a2d1f0b4212a7
Mike Nguyen <32407498+mikeybear@users.noreply.github.com>
2019-12-09 10:20:05 -0800

    changed database port and tweaked downloads

--- hub.php
+++ hub.php
@@ -12,7 +12,8 @@
             <li>Lackluster Library Locator</li>
             <li>2nd floor</li>
             <li><?php
-            $db_connection = pg_connect("host=10.34.44.133 dbname=testdb user=postgres password=library2") // host=localhost
+            $db_connection = pg_connect("host=24.7.172.204 dbname=testdb user=postgres password=library2") // host=localhost dbname=Library, testdb
+				// 10.34.44.133, 10.0.0.220
             or die('Could not connect: ' . pg_last_error());
             
 			$query = 'SELECT SUM(movein) FROM locationtimeinfo, locationid WHERE locationtimeinfo.id = locationid.id AND locationid.id = 1';
@@ -38,82 +39,25 @@
             pg_close($db_connection);
             ?></li>
 <li><form name="form1" method="post" action="csv.php">
-<label class="logoutButton">
 <input name="submit2" type="submit" id="submit2" value="Download">
-</label>
 </form></li>
             <hr><br>
             <li>3rd floor</li>
-<li><?php
-	$db_connection = pg_connect("host=10.34.44.133 dbname=testdb user=postgres password=library2")
-	or die('Could not connect: ' . pg_last_error());
-	
-	$query = 'SELECT SUM(movein) FROM locationtimeinfo, locationid WHERE locationtimeinfo.id = locationid.id AND locationid.id = 2';
-	$usercomp = pg_query($query) or die('Query failed: ' . pg_last_error());
-	$query1 ='SELECT SUM(moveout) FROM locationtimeinfo, locationid WHERE locationtimeinfo.id = locationid.id AND locationid.id = 2';
-	$usercomp1 = pg_query($query1) or die('Query failed: ' . pg_last_error());
-	
-	// output
-	echo "Current people coming in: ";
-	while ($line = pg_fetch_array($usercomp, null, PGSQL_ASSOC))  {
-		foreach ($line as $col_value) {
-			echo "\t\t<td>$col_value</td>\n";
-		}
-		echo "<br><br>";
-	}
-	echo "Current people coming out: ";
-	while ($line = pg_fetch_array($usercomp1, null, PGSQL_ASSOC))  {
-		foreach ($line as $col_value) {
-			echo "\t\t<td>$col_value</td>\n";
-		}
-	}
-	// close connection
-	pg_close($db_connection);
-	?></li>
+<li></li>
 <li><li><form name="form1" method="post" action="csv.php">
-<label class="logoutButton">
 <input name="submit2" type="submit" id="submit2" value="Download">
-</label>
 </form></li></li>
             <hr><br>
             <li>4th floor</li>
-<li><?php
-	$db_connection = pg_connect("host=10.34.44.133 dbname=testdb user=postgres password=library2")
-	or die('Could not connect: ' . pg_last_error());
-	
-	$query = 'SELECT SUM(movein) FROM locationtimeinfo, locationid WHERE locationtimeinfo.id = locationid.id AND locationid.id = 3';
-	$usercomp = pg_query($query) or die('Query failed: ' . pg_last_error());
-	$query1 ='SELECT SUM(moveout) FROM locationtimeinfo, locationid WHERE locationtimeinfo.id = locationid.id AND locationid.id = 3';
-	$usercomp1 = pg_query($query1) or die('Query failed: ' . pg_last_error());
-	
-	// output
-	echo "Current people coming in: ";
-	while ($line = pg_fetch_array($usercomp, null, PGSQL_ASSOC))  {
-		foreach ($line as $col_value) {
-			echo "\t\t<td>$col_value</td>\n";
-		}
-		echo "<br><br>";
-	}
-	echo "Current people coming out: ";
-	while ($line = pg_fetch_array($usercomp1, null, PGSQL_ASSOC))  {
-		foreach ($line as $col_value) {
-			echo "\t\t<td>$col_value</td>\n";
-		}
-	}
-	// close connection
-	pg_close($db_connection);
-	?></li>
+<li></li>
 <li><li><form name="form1" method="post" action="csv.php">
-<label class="logoutButton">
 <input name="submit2" type="submit" id="submit2" value="Download">
-</label>
 </form></li></li>
             <hr><br>
+<li>
 <form name="form1" method="post" action="login.php">
-<label class="logoutButton">
 <input name="submit2" type="submit" id="submit2" value="Log out">
-</label>
-</form>
+</form></li>
         </ul>
         </div>
     </body>
